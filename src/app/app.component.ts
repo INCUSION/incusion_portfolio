b405ff9c917fb2bdbe490efe2719e350624e3e6f
Domenico Winkelmann <domiwink20@gmail.com>
2020-10-01 22:28:48 +0200

    added home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-root',
@@ -9,39 +8,5 @@ import anime from 'animejs/lib/anime.es.js';
 export class AppComponent {
   title = 'incusion-portfolio';
 
-  ngAfterViewInit(): void {
-
-    // Wrap every letter in a span
-    var textWrapper = document.querySelector('.header .letters');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-    
-    anime.timeline({loop: true})
-      .add({
-        targets: '.header .line',
-        scaleY: [0,1],
-        opacity: [0.5,1],
-        easing: "easeOutExpo",
-        duration: 700
-      })
-      .add({
-        targets: '.header .line',
-        translateX: [0, document.querySelector('.header .letters').getBoundingClientRect().width + 10],
-        easing: "easeOutExpo",
-        duration: 700,
-        delay: 100
-      }).add({
-        targets: '.header .letter',
-        opacity: [0,1],
-        easing: "easeOutExpo",
-        duration: 600,
-        offset: '-=775',
-        delay: (el, i) => 34 * (i+1)
-      }).add({
-        targets: '.header',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-      });
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryComponent
+    GalleryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
